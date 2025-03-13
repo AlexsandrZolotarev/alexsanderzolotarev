6c8c9a5f28125da38b50473985c08e8ad3bef979
Alexsandr Zolotarev <380788-500@mail.ru>
2025-03-13 18:04:37 +0300

    3 method . remove black screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
+import Loader from "./Loader/loader";
 import Portfolio from "./Portfolio/Portfolio";
 import Skills from "./Skills/Skills";
 import "./styles/main.scss";
 function App() {
   return (
     <>
+    {/* <Loader/> */}
       <Header />
       <main>
         <Hero/>
@@ -14,6 +16,7 @@ function App() {
         <Portfolio/>
       </main>
       <Footer/>
+      
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Loader from './Loader/loader';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Loader/>
     <App/>
   </React.StrictMode>
 );
